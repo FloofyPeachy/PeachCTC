ffa0d085d997c8f164128571582e101402c654d3
mensahkenny76 <mensahkenny76@gmail.com>
2017-12-24 17:57:58 -0500

    Welcome to Chillies1

--- daserver.js
+++ daserver.js
@@ -1,18 +1,26 @@
-var port = 8080;
-var http = require("http");
-var server = http.createServer();
-server.on('request', request);
-server.listen(port);
-function request(request, response) {
-    var store = '';
+var http = require('http'),
+      fs = require('fs'),
+     url = require('url'),
+ choices = ["hello world", "goodbye world"];
 
-    request.on('data', function(data) 
-    {
-        store += data;
-    });
-    request.on('end', function() 
-    {  console.log(store);
-        response.write("Recived, thanks!")
-        response.end()
-    });
- }  
+http.createServer(function(request, response){
+    var path = url.parse(request.url).pathname;
+    if(path=="/boardupdate"){
+        console.log("request recieved");
+        var string = choices[Math.floor(Math.random()*choices.length)];
+        console.log("string '" + string + "' chosen");
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.end(string);
+        console.log("string sent");
+    }else{
+        fs.readFile('./index.html', function(err, file) {  
+            if(err) {  
+                // write an error response or nothing here  
+                return;  
+            }  
+            response.writeHead(200, { 'Content-Type': 'text/html' });  
+            response.end(file, "utf-8");  
+        });
+    }
+}).listen(8001);
+console.log("server initialized");
